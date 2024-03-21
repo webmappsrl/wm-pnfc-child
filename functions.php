311cf5dd936faa146300be6e23b0514258f2060b
Rubens Garofalo <rubensgiuseppegarofalo@gmail.com>
2024-03-21 16:32:38 +0100

    cleaning single track code

--- functions.php
+++ functions.php
@@ -1,6 +1,6 @@
 <?php
 // Exit if accessed directly
-if ( !defined( 'ABSPATH' ) ) exit;
+if (!defined('ABSPATH')) exit;
 
 require('shortcodes/single_track.php');
 require('shortcodes/single_poi.php');
@@ -11,13 +11,22 @@ require('shortcodes/single_layer.php');
 // BEGIN ENQUEUE PARENT ACTION
 // AUTO GENERATED - Do not modify or remove comment markers above or below:
 
-if ( !function_exists( 'chld_thm_cfg_locale_css' ) ):
-    function chld_thm_cfg_locale_css( $uri ){
-        if ( empty( $uri ) && is_rtl() && file_exists( get_template_directory() . '/rtl.css' ) )
+if (!function_exists('chld_thm_cfg_locale_css')) :
+    function chld_thm_cfg_locale_css($uri)
+    {
+        if (empty($uri) && is_rtl() && file_exists(get_template_directory() . '/rtl.css'))
             $uri = get_template_directory_uri() . '/rtl.css';
         return $uri;
     }
 endif;
-add_filter( 'locale_stylesheet_uri', 'chld_thm_cfg_locale_css' );
+add_filter('locale_stylesheet_uri', 'chld_thm_cfg_locale_css');
 
 // END ENQUEUE PARENT ACTION
+
+//Swiper Slider CSS da CDN
+function child_theme_enqueue_swiper()
+{
+    wp_enqueue_style('swiper-css', 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css');
+    wp_enqueue_script('swiper-js', 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js', array(), null, true);
+}
+add_action('wp_enqueue_scripts', 'child_theme_enqueue_swiper');
--- shortcodes/single_track.php
+++ shortcodes/single_track.php
@@ -1,195 +1,148 @@
 <?php
 if (!is_admin()) {
-    add_shortcode('wm_single_track', 'wm_single_track_pnfc');
+	add_shortcode('wm_single_track', 'wm_single_track_pnfc');
 }
 
 function wm_single_track_pnfc($atts)
 {
-    if (defined('ICL_LANGUAGE_CODE')) {
-        $language = ICL_LANGUAGE_CODE;
-    } else {
-        $language = 'it';
-    }
-    extract(shortcode_atts(array(
-        'track_id' => '',
-        'activity' => ''
-    ), $atts));
+	if (defined('ICL_LANGUAGE_CODE')) {
+		$language = ICL_LANGUAGE_CODE;
+	} else {
+		$language = 'it';
+	}
+	extract(shortcode_atts(array(
+		'track_id' => '',
+	), $atts));
 
-    // <iframe src="https://geohub.webmapp.it/w/simple/16294" frameborder="0"></iframe>
-    $single_track_base_url = get_option('track_url');
-    $geojson_url = $single_track_base_url.$track_id;
+	// <iframe src="https://geohub.webmapp.it/w/simple/16294" frameborder="0"></iframe>
+	$single_track_base_url = get_option('track_url');
+	$geojson_url = $single_track_base_url . $track_id;
 
-    $track = json_decode(file_get_contents($geojson_url), true);
-    $track = $track['properties'];
-    // echo '<pre>';
-    // print_r($track);
-    // echo '</pre>';
-    $excerpt = null;
-    if (array_key_exists('excerpt', $track) && array_key_exists($language, $track['excerpt'])) {
-        $excerpt = $track['excerpt'][$language];
-    }
-    $description = $track['description'][$language];
-    $title = $track['name'][$language];
-    $featured_image = get_stylesheet_directory_uri().'/assets/images/background.jpg';
-    // echo '<pre>';
-    // print_r($featured_image);
-    // echo '</pre>';
-    $featured_image = $track['feature_image']['url'];
-    $featured_image = $track['feature_image']['sizes']['1440x500'];
-    $gallery = array_key_exists('imageGallery', $track) ? $track['imageGallery'] : null;
-    $gpx = $track['gpx_url'];
+	$track = json_decode(file_get_contents($geojson_url), true);
+	$track = $track['properties'];
+	// echo '<pre>';
+	// print_r($track);
+	// echo '</pre>';
+	$excerpt = null;
+	if (array_key_exists('excerpt', $track) && array_key_exists($language, $track['excerpt'])) {
+		$excerpt = $track['excerpt'][$language];
+	}
+	$description = $track['description'][$language];
+	$title = $track['name'][$language];
+	$featured_image = get_stylesheet_directory_uri() . '/assets/images/background.jpg';
+	// echo '<pre>';
+	// print_r($featured_image);
+	// echo '</pre>';
+	$featured_image = $track['feature_image']['url'];
+	$featured_image = $track['feature_image']['sizes']['1440x500'];
+	$gallery = array_key_exists('image_gallery', $track) ? $track['image_gallery'] : null;
+	$gpx = $track['gpx_url'];
 
-    $mapping = array();
-    // mapping the tickets section
-    foreach ($mapping_tickets as $track => $info) {
-        if (strtolower($track) == strtolower($track_id . '_' . $language)) {
-            $mapping = $info;
-        }
-    }
-    ob_start();
-    ?>
+	$mapping = array();
 
-<section class="l-section wpb_row height_small wm_track_breadcrumb_section">
-	<div class="l-section-h i-cf">
-		<div class="pm-breadcrumb-yoast">
-			<div class="wpb_wrapper">
-				<?php echo do_shortcode('[wpseo_breadcrumb]'); ?>
-			</div>
-		</div>
-	</div>
-</section>
-<section class="l-section wpb_row height_small with_img with_overlay wm_track_header_section">
+	ob_start();
+?>
 
-	<div class="l-section-img loaded pm-header-image"
-		style="background-image: url(<?= $featured_image ?>);background-repeat: no-repeat;">
-	</div>
-	<div class="l-section-overlay"
-		style="background: linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.3) 100%)"></div>
-	<div class="l-section-h i-cf wm_track_header_wrapper">
-		<h3 class="align_left wm_track_header_taxonomy">
-			<?= (($language == 'it') ? __('Itinerary', 'wm-pnfc-child') . ' ' : '') . $activity ?>
-		</h3>
-		<h1 class="align_left wm_track_header_title">
-			<?= $title ?>
-		</h1>
-	</div>
-</section>
-<div class="wm_track_body_section">
-	<div class="wm_track_body_map_wrapper">
-		<div class="wm_track_body_map_title">
-			<?php if ($excerpt) { ?>
-			<div class="wm_track_body_excerpt">
-				<h2><?php echo $excerpt; ?></h2>
-			</div>
-			<?php } ?>
-		</div>
-		<div class="wm_track_body_map">
-			<?php
-                    echo do_shortcode('[wm-embedmaps geojson_url="' . $geojson_url . '" height="500px" lang="' . $language . '" related_poi_click_behaviour="open" show_related_pois="true" fullscreen="true"  hide_taxonomy_filters="true"]');
-    ?>
-		</div>
-	</div>
-	<?php if ($description) { ?>
-	<div class="wm_track_body_description">
-		<?php echo $description; ?>
-	</div>
-	<?php } ?>
-	<div class="wm_track_body_gallery">
-		<?php if (is_array($gallery) && !empty($gallery)) : ?>
-		<div class="slick-slider">
-			<?php foreach ($gallery as $image) : ?>
-			<div>
-				<img src="<?= esc_url($image['sizes']['400x200']) ?>"
-					alt="" loading="lazy">
-			</div>
-			<?php endforeach; ?>
-		</div>
-		<?php endif; ?>
-	</div>
-	<div class="wm_track_body_sidebar_wrapper">
-		<div class="wm_track_body_map_details">
-			<div class="wm_track_detail">
-				<p class="track_sidebar_label">
-					<?= __('Tecnical info', 'wm-child-maremma') ?>
-				</p>
-			</div>
-			<?php
-    echo do_shortcode('[wm-embedmaps-technical-info feature_id="' . $track_id . '-' . $track_id . '" config="ele_from,ele_to,ele_max,ele_min,distance,duration_forward,ascent,descent,difficulty,scale"]');
-    ?>
-			<div class="wm_track_body_map_elevation">
-				<p class="track_sidebar_label">
-					<?= __('Elevation chart', 'wm-child-maremma') ?>
-				</p>
-				<div class="wm-elevation-chart">
-					<?php
-            echo do_shortcode('[wm-embedmaps-elevation-chart feature_id="' . $track_id . '-' . $track_id . '"]');
-    ?>
+	<section class="l-section wpb_row height_small wm_track_breadcrumb_section">
+		<div class="l-section-h i-cf">
+			<div class="pm-breadcrumb-yoast">
+				<div class="wpb_wrapper">
+					<?php echo do_shortcode('[wpseo_breadcrumb]'); ?>
 				</div>
 			</div>
-			<div class="wm_track_body_download">
-				<a class="icon_atleft"
-					href="<?= $gpx ?>"><?= __('Download GPX', 'wm-child-maremma') ?></span></a>
+		</div>
+	</section>
+	<section class="l-section wpb_row height_small with_img with_overlay wm_track_header_section">
+		<div class="l-section-img loaded wm-header-image" style="background-image: url(<?= $featured_image ?>);background-repeat: no-repeat;">
+		</div>
+		<div class="l-section-h i-cf wm_track_header_wrapper">
+		</div>
+	</section>
+	<div class="wm_track_body_section">
+		<div class="wm_track_body_map_wrapper">
+			<h1 class="align_left wm_track_header_title">
+				<?= $title ?>
+			</h1>
+			<div class="wm_track_body_map">
+				<?php
+				echo do_shortcode('[wm-embedmaps geojson_url="' . $geojson_url . '" height="500px" lang="' . $language . '" related_poi_click_behaviour="open" show_related_pois="true" fullscreen="true"  hide_taxonomy_filters="true"]');
+				?>
+			</div>
+		</div>
+		<?php if ($description) { ?>
+			<div class="wm_track_body_description">
+				<?php echo $description; ?>
+			</div>
+		<?php } ?>
+		<div class="wm_track_body_gallery">
+			<?php if (is_array($gallery) && !empty($gallery)) : ?>
+				<!-- Swiper -->
+				<div class="swiper-container">
+					<div class="swiper-wrapper">
+						<?php foreach ($gallery as $image) : ?>
+							<div class="swiper-slide">
+								<img src="<?= esc_url($image['sizes']['400x200']) ?>" alt="" loading="lazy">
+							</div>
+						<?php endforeach; ?>
+					</div>
+					<!-- Add Pagination -->
+					<div class="swiper-pagination"></div>
+					<!-- Add Navigation -->
+					<div class="swiper-button-prev"></div>
+					<div class="swiper-button-next"></div>
+				</div>
+			<?php endif; ?>
+		</div>
+
+
+		<div class="wm_track_body_sidebar_wrapper">
+			<div class="wm_track_body_map_details">
+				<div class="wm_track_detail">
+					<p class="track_sidebar_label">
+						<?= __('Tecnical info', 'wm-child-maremma') ?>
+					</p>
+				</div>
+				<?php
+				echo do_shortcode('[wm-embedmaps-technical-info feature_id="' . $track_id . '-' . $track_id . '" config="ele_from,ele_to,ele_max,ele_min,distance,duration_forward,ascent,descent,difficulty,scale"]');
+				?>
+				<div class="wm_track_body_map_elevation">
+					<p class="track_sidebar_label">
+						<?= __('Elevation chart', 'wm-child-maremma') ?>
+					</p>
+					<div class="wm-elevation-chart">
+						<?php
+						echo do_shortcode('[wm-embedmaps-elevation-chart feature_id="' . $track_id . '-' . $track_id . '"]');
+						?>
+					</div>
+				</div>
+				<div class="wm_track_body_download">
+					<a class="icon_atleft" href="<?= $gpx ?>"><?= __('Download GPX', 'wm-child-maremma') ?></span></a>
+				</div>
 			</div>
 		</div>
 	</div>
-	<div class="wm_track_body_content_wrapper">
-		<?php
-            if (!empty($mapping)) {
-                ?>
-		<div class="wm_track_body_ticket">
-			<p class="ticket_text">
-				<?= $mapping['description'] ?>
-			</p>
-			<?php if (array_key_exists('calendar', $mapping)) { ?>
-			<div class="single_track_ticket_btn">
-				<a class="w-btn us-btn-style_1"
-					href="<?= $mapping['calendar'] ?>"><span
-						class="w-btn-label"><?= __('Go to calendar', 'wm-child-maremma') ?></span></a>
-			</div>
-			<a class="single_track_ticket_link"
-				href="<?= $mapping['subscription'] ?>"><span
-					class="w-btn-label"><?= __('Subscription and promotions', 'wm-child-maremma') ?></span></a>
-			<?php
-			}
-                ?>
-		</div><?php
-            }
-    if (array_key_exists('purchase', $mapping)) {
-        ?>
-		<div class="single_track_ticket_btn">
-			<a class="w-btn us-btn-style_6 purchase-button"
-				href="<?= esc_url($mapping['purchase']); ?>">
-				<span
-					class="w-btn-label"><?= esc_html__('Purchase', 'wm-child-maremma'); ?></span>
-				<img src="/wp-content/uploads/2023/11/Tracciato-95.png" alt="Arrow Icon" class="pm-arrow-icon">
-			</a>
-		</div>
-		<?php
-    }
-    if (array_key_exists('subscription', $mapping)) {
-        ?><?php
-    }
-    ?>
-	</div>
 
-</div>
-
-<script type="text/javascript">
-	jQuery(document).ready(function($) {
-		$('.slick-slider').slick({
-			dots: false,
-			infinite: true,
-			speed: 300,
-			slidesToShow: 1,
-			adaptiveHeight: false,
-			variableWidth: false,
-			centerMode: false,
+	<script>
+		document.addEventListener('DOMContentLoaded', function() {
+			var swiper = new Swiper('.swiper-container', {
+				loop: true,
+				autoplay: {
+					delay: 5000,
+				},
+				pagination: {
+					el: '.swiper-pagination',
+					clickable: true,
+				},
+				navigation: {
+					nextEl: '.swiper-button-next',
+					prevEl: '.swiper-button-prev',
+				},
+			});
 		});
-	});
-</script>
+	</script>
 
 <?php
 
-    return ob_get_clean();
+	return ob_get_clean();
 }
 ?>
